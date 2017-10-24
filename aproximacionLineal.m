52709841518d4e5451862da38e8639442b84dabd
Erik-1012 <bernaderi@hotmail.com>
2017-10-24 04:36:07 -0300

    Hago aproximación por parábola y tuneo un toque la lineal

--- aproximacionLineal.m
+++ aproximacionLineal.m
@@ -1,11 +1,11 @@
 function [m,b]=aproximacionLineal(X) 
 %X es la matriz a utilizar%
-n=length(X(1,:)); 
-A=0; 
-B=0; 
-C=0; 
-D=0; 
 
+n=length(X(1,:)); 
+A=0;%Es la sumatoria de Xi 
+B=0;%Es la sumatoria de Yi
+C=0;%Es la sumatoria de Xi al cuadrado
+D=0;%Es la sumatoria de Yi*Xi
 
 for i=1:n; 
     A=A+X(1,i); 
@@ -14,9 +14,8 @@ for i=1:n;
     D=D+X(1,i)*X(2,i); 
 end 
 
-
-m=(n*D-A*B)/(n*C-A^2); 
-b=(C*B-D*A)/(n*C-A^2); 
+m=(n*D-A*B)/(n*C-A^2);
+b=(C*B-D*A)/(n*C-A^2);
  
 
 for i=1:n; 
@@ -24,11 +23,12 @@ for i=1:n;
     plot (X(1,i),X(2,i),'*','MarkerEdgeColor','r','LineWidth',1); 
 end 
 
+%Extremos a considerar para el gráfico
+e1 =-X(1,1);
+e2=X(1,n);
 
-x=X(1,1):1:X(1,n); 
-y=m*x+b; 
-plot(x,y,'b'); 
+%Grafico de la recta obtenida
+fplot(@(x) m*x + b,[e1 e2],'b')
 title('Aproximación lineal por mínimos cuadrados');
 
 end
-
