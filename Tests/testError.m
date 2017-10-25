742a9736fe16aa2a7654b5f67d31d08d78e3df11
SLDG1297 <santiagoluccadeguevara@gmail.com>
2017-10-25 16:59:37 -0300

    Acomodo las cosas. Arreglo la escritura de los test de error. Queda en Nachito arreglarlo

--- Tests/testError.m
+++ Tests/testError.m
@@ -2,12 +2,16 @@ function tests = testError
     tests = functiontests(localfunctions);
 end
 
-function testCalcularError()
+function testCalcularError(casoDePrueba)
 
     [m , b] = aproximacionLineal([ 1 2 3 4 ; 10 5 2 1 ]);
     
     matriz = [ 1 2 3 4 ; 10 5 2 1 ];
     
-    calcularError([m , b], matriz);
+    actual = calcularError([m , b], matriz);
+    
+    esperado = 4;
+    
+    verifyEqual(casoDePrueba, actual, esperado, 'RelTol', 0.0001);
 
 end
